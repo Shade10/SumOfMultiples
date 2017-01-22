5e35620853113f4757c0bbbe415ff13b851b7ba8
Marcin Kass <marcin.kass10@gmail.com>
2017-01-22 20:40:26 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_multiples
+{
+    public class Class1
+    {
+    }
+}
