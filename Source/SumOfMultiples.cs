1bcd3bd1dc1561823ccda58c5f2efaf7138c1263
Marcin Kass <marcin.kass10@gmail.com>
2017-01-23 10:40:41 +0100

    task done

--- Source/SumOfMultiples.cs
+++ Source/SumOfMultiples.cs
@@ -8,6 +8,12 @@ namespace sum_of_multiples
 {
     public class SumOfMultiples
     {
-
+        public static int To(int[] factors, int limit)
+        {
+            return Enumerable
+                .Range(0, limit)
+                .Aggregate(0, (sum, n) =>
+                    factors.Any((m) => n % m == 0) ? sum + n : sum);
+        }
     }
 }
